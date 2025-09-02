557b0c1a5bb8b1d4e8baf186a10ba43a00285b1f
Alex <aliakseych@gmail.com>
2025-09-02 21:11:16 +0500

    fix: fixed logic for 'handleUnhandled' function. Сhanged the code to mostly use custom Request class, not the node:http one. Changed some log messages to debug level

--- src/server/dispatcher.ts
+++ src/server/dispatcher.ts
@@ -3,6 +3,8 @@ import http from "node:http"
 import { Router } from "./router.js"
 import { Logger } from "../logger.js"
 import type { Header } from "./header.js"
+import type { Request } from "./request.js"
+import type { HandlerResponse } from "./response.js"
 
 export { Dispatcher }
 
@@ -30,11 +32,26 @@ class Dispatcher extends Router {
 
     public async run(port: number = 8000, hostname: string = "localhost"): Promise<void> {
         this.server = http.createServer(
-            // Handling the requesjs via handleEvent function
-            async (request: http.IncomingMessage, response: http.ServerResponse) => {
-                const eventHandled: boolean = await this.handleEvent(request, response)
-                if (eventHandled == false) {
-                    await this.handleUnhandled(request, response)
+            // Handling the request via handleEvent function
+            async (clientRequest: http.IncomingMessage, response: http.ServerResponse) => {
+                const requestPath: string = this.getRequestPath(clientRequest)
+                let request: Request | undefined | null = {
+                    clientRequest: clientRequest,
+                    args: {},
+                    path: requestPath
+                }
+
+                const eventHandled: Request | boolean = await this.handleEvent(request, response)
+
+                // If event hasn't been declined or handled
+                if (typeof eventHandled !== "boolean") {
+                    const handlerResponse: HandlerResponse | void = await this.handleUnhandled(eventHandled)
+
+                    // If function, responsible for handling unhandled requests doesn't respond with
+                    // handler response - return nothing
+                    if (handlerResponse) {
+                        await this.sendResponse(response, handlerResponse, requestPath)
+                    }
                 }
             }
         )
@@ -55,5 +72,5 @@ class Dispatcher extends Router {
     }
 
     // Empty function, that can be replaced by user's business logic for handling unhandled responses
-    public async handleUnhandled(request: http.IncomingMessage, response: http.ServerResponse): Promise<void> {return}
+    public async handleUnhandled(request: Request): Promise<HandlerResponse | void> {return}
 }
